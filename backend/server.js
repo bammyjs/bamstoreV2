0360643e704686be2616d885804a618e8ee39820
bammytech1 <66552185+bammytech1@users.noreply.github.com>
2024-03-10 17:05:09 +0000

    dfhfrjtyjtyjtej

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(
       "https://bamstore-store.onrender.com",
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
 
